7599adb519c8cf5b0d8fed268c3d898adfdfa7d4
Suleiman SLeiz <suleimansleiz@gmail.com>
2024-07-01 11:32:26 +0300

    second commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log('yo sleiz!');
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("this is a get request");
+});
+app.post("/", (req, res) => {
+  res.send("this is a post request");
+});
+app.listen(PORT, () => {
+  console.log("app listening to port " + PORT);
+});
+
+const start = async() => {
+  await mongoose.connect('mongodb+srv://suleimansleiz:suleiman@cluster0.17avjjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+  app.listen(PORT, () => {
+  console.log("app listening to port " + PORT);
+});
+};
+
+start();
